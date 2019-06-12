5a6b8f5d21e2c05602873da61dc437590c081ab6
lsy563193 <lsy563193@gmail.com>
2019-06-12 14:07:41 +0800

    twoSum

--- algorithms/cpp/twoSum/twoSum.cpp
+++ algorithms/cpp/twoSum/twoSum.cpp
@@ -48,37 +48,17 @@ public:
     //  2) just put the `target - num[i]`(not `num[i]`) into the map
     //     so, when we checking the next num[i], if we found it is exisited in the map.
     //     which means we found the second one.
-    //      
+    //
+    //  l,r = v[l] + v[r] =  target;
     vector<int> twoSum(vector<int> &numbers, int target) {
-        unordered_map<int, int> m;
-        vector<int> result;
-        for(int i=0; i<numbers.size(); i++){
-            // not found the second one
-            if (m.find(numbers[i])==m.end() ) { 
-                // store the first one poisition into the second one's key
-                m[target - numbers[i]] = i; 
-            }else { 
-                // found the second one
-                result.push_back(m[numbers[i]]);
-                result.push_back(i);
-                break;
+        for(auto l = 0; l< numbers.size();  ++l)
+        {
+            for(auto r = l+1; r< numbers.size();  ++r)
+            {
+                if(numbers[l] + numbers[r] == target)
+                    return  {l,r};
             }
         }
-        return result;
+        return {};
     }
-
-    // we also can store nums[i] into map, and find target - nums[i]
-//    vector<int> twoSum(vector<int>& nums, int target) {
-//        unordered_map<int, int> m;
-//        vector<int> result;
-//        for (int i=0; i<nums.size(); i++) {
-//            if ( m.find(target - nums[i]) == m.end() ) {
-//                m[nums[i]] = i;
-//            }else{
-//                result.push_back(m[target - nums[i]]);
-//                result.push_back(i);
-//            }
-//        }
-//        return result;
-//    }
 };
--- algorithms/cpp/twoSum/twoSum_test.cpp
+++ algorithms/cpp/twoSum/twoSum_test.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <map>
 #include <unordered_map>
+#include <chrono>
 #include "gmock/gmock.h"
 
 using namespace std;
@@ -15,18 +16,36 @@ TEST(twoSum, Solution)
     Solution solution;
     auto result = solution.twoSum(num, target);
 
+    ASSERT_EQ(result.size(), 2);
     ASSERT_EQ(result[0], 0);
     ASSERT_EQ(result[1], 1);
 
     target = 18;
     result = solution.twoSum(num, target);
-
     ASSERT_EQ(result[0], 1);
     ASSERT_EQ(result[1], 2);
 
     target = 22;
     result = solution.twoSum(num, target);
-
     ASSERT_EQ(result[0], 1);
     ASSERT_EQ(result[1], 3);
+
+    target = 23;
+    result = solution.twoSum(num, target);
+    ASSERT_EQ(result.size(), 0);
+//    ASSERT_EQ(result[0], 1);
+//    ASSERT_EQ(result[1], 3);
+}
+
+TEST(twoSum, time)
+{
+    vector<int> num(20000);
+    Solution solution;
+    auto target = 23;
+    auto start = chrono::system_clock::now();
+    auto result = solution.twoSum(num, target);
+    auto end = chrono::system_clock::now();
+    auto duration = chrono::duration_cast<chrono::seconds>(end-start);
+    cout << duration.count() <<std::endl;
+    ASSERT_EQ(result.size(), 0);
 }
